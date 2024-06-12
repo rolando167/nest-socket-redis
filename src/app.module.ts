c64dcbcdc94f4232dfe222499e0fc353a27d74d5
Rolando HR <rolando167@hotmail.com>
2024-06-12 11:02:09 +0200

    RH- ca va

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ModulesModule } from './modules';
 import { RouterModule } from '@nestjs/core';
 import { appRoutes } from './app.routes';
+import { CacheModule } from '@nestjs/cache-manager';
 // import { AppController } from './app.controller';
 // import { AppService } from './app.service';
 
@@ -9,6 +10,9 @@ import { appRoutes } from './app.routes';
     imports: [
         ModulesModule,
         RouterModule.register(appRoutes),
+        CacheModule.register({
+            isGlobal: true,
+        })
     ],
     // controllers: [AppController],
     // providers: [AppService],
--- src/modules/Tasks/task.service.ts
+++ src/modules/Tasks/task.service.ts
@@ -1,14 +1,25 @@
-import { Injectable } from "@nestjs/common";
+import { Inject, Injectable } from "@nestjs/common";
 import * as MOCKED_USERS from '../../mocks/data/users.json';
 import { sleep } from "src/utils/sleep";
+import { CACHE_MANAGER } from "@nestjs/cache-manager";
+import { Cache } from "cache-manager";
 
 @Injectable()
 export class TaskService {
 
-    async getTasks() {
-        const allTasks: any[] = MOCKED_USERS;
+    constructor(@Inject(CACHE_MANAGER) private cache_manager: Cache){}
 
+    async getTasks() {
+        const key = 'user-find-all';
+        const userCache = await this.cache_manager.get(key);
+
+        if(userCache){
+            return userCache;
+        }
         await sleep(3000)
-        return allTasks;
+        const allUsers: any[] = MOCKED_USERS;
+
+        await this.cache_manager.set(key, allUsers, 1000 * 10)
+        return allUsers;
     }
 }
